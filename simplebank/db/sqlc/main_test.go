d382f150fa8fb9e2cc24d5e22cea2d6dcb4fe75c
Phadoo131 <138201998+Phadoo131@users.noreply.github.com>
2023-07-20 18:26:59 +0700

    Add files via upload

--- simplebank/db/sqlc/main_test.go
+++ simplebank/db/sqlc/main_test.go
@@ -9,6 +9,7 @@ import (
 	_"github.com/lib/pq"
 )
 var testQuerie *Queries
+var testDB *sql.DB
 
 const (
 	dbDriver = "postgres"
@@ -16,12 +17,14 @@ const (
 )
 
 func TestMain(m *testing.M){
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to the DB: ", err)
 	}
 
-	testQuerie = New(conn)
+	testQuerie = New(testDB)
 
 	os.Exit(m.Run())
 }
